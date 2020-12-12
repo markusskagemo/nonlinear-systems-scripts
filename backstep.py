f83cc37d74e1b4b5657b99c5394e8174437c8ca1
saturdayquant <markusskagemo@gmail.com>
2020-12-13 00:00:32 +0100

    infer function components in backstepping

--- backstep.py
+++ backstep.py
@@ -12,26 +12,20 @@ f, g, h, V_1, V_2, phi = symbols('f g h V_1 V_2 phi')
 
 if __name__ == '__main__':
     # Specify system and LF
-    x_1dot = exp(x_1) - x_1**3 - 1 + x_2
-    x_2dot = -x_2**3 + u
-        
-    """f = exp(x_1) - x_1**3 - 1
-    g = 1
-    h = -x_2**3"""
-    # > TODO: recognize g, f, h automatically
-    #p = Wild('p')
+    x_1dot = 1/exp(x_1) - 2*x_1**3 - 1 + x_2*x_1/100
+    x_2dot = -x_2**5 + u + 1
+    V_1 = x_1**2/2
+    
+    # Infer function components f, g, h from system
     g = x_1dot.as_independent(x_2)[1]/x_2
     f = x_1dot - g * x_2
     h = x_2dot.as_independent(x_2)[1]
     print('Verify inferred function components:')
     print('f = {}, g = {}, h = {}'.format(f, g, h))
     
-    # Specify Lyapunov function
-    V_1 = x_1**2/2
-
     V_1dot = diff(V_1, x_1) * (f + phi * g)
     # Choose phi so \dot{V_1} < 0
-    phi = k_1 * solve(V_1dot < 0, phi).rhs
+    phi = k_1 * solve(V_1dot < 0, phi).rhs # > TODO: Improve
     phi_dot = diff(phi, x_1)
     assert x_2 not in phi.free_symbols, 'x_2 should not be in \phi(x_1)'
     assert x_2 not in phi_dot.free_symbols, 'x_2 should not be in \dot{\phi}(x_1)'
@@ -46,6 +40,12 @@ if __name__ == '__main__':
     V_2 = V_1 + V_1.subs(x_1, z)
     V_2dot = x_1*(f + g*phi) - k_2*z**2
     V_2dot = V_2dot.subs(u, inp)
+    
+    # Weak definiteness tests
+    if len(solve(V_2dot == 0, (x_1, x_2, z))) > 1:
+        raise ValueError('V_dot is not negative def.', V_dot)
+    if len(solve(V_2 == 0, (x_1, x_2))) > 1:
+        raise ValueError('V is not positive def.', V)
 
     plt_texts = ['$\phi(x_1) = %s$' % latex(phi)]
     plt_texts.append('$\dot{\phi}(x_1) = %s$' % latex(phi_dot))
@@ -54,7 +54,6 @@ if __name__ == '__main__':
     plt_texts.append('$V_2 = %s$' % latex(simplify(V_2)))
     plt_texts.append('$V_2 = %s$' % latex(simplify(V_2).subs(z, x_2 - phi)))
     plt_texts.append('$\dot{V}_2 = %s$' % latex(simplify(V_2dot)))
-    print(plt_texts)
     
     # Render LaTeX
     ax = plt.axes([0, 0, 0.1, 0.1])
