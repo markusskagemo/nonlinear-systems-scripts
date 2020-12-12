cacda1c3fb13a80943d1e21ed6be83e6a7da75d2
saturdayquant <markusskagemo@gmail.com>
2020-12-13 00:12:05 +0100

    fixed g infer bug

--- backstep.py
+++ backstep.py
@@ -1,5 +1,5 @@
 from sympy.parsing.latex import parse_latex
-from sympy import symbols, diff, exp, simplify, expand, latex, solve
+from sympy import symbols, diff, exp, simplify, expand, latex, solve, atan
 from sympy import Function, Wild
 
 import matplotlib.pyplot as plt
@@ -12,21 +12,23 @@ f, g, h, V_1, V_2, phi = symbols('f g h V_1 V_2 phi')
 
 if __name__ == '__main__':
     # Specify system and LF
-    x_1dot = 1/exp(x_1) - 2*x_1**3 - 1 + x_2*x_1/100
-    x_2dot = -x_2**5 + u + 1
+    x_1dot = 1/exp(x_1) - 2*x_1**3 - 1 + x_2/x_1**2 + x_2*x_1**3
+    x_2dot = -x_2**5 + u + 1 + exp(atan(x_2))
     V_1 = x_1**2/2
     
     # Infer function components f, g, h from system
-    g = x_1dot.as_independent(x_2)[1]/x_2
-    f = x_1dot - g * x_2
-    h = x_2dot.as_independent(x_2)[1]
+    g_unsub = x_1dot.as_independent(x_2)[1]
+    g = g_unsub.subs({x_2: 1})
+    f = x_1dot - g_unsub
+    h = x_2dot - x_2dot.as_independent(u)[1]
     print('Verify inferred function components:')
-    print('f = {}, g = {}, h = {}'.format(f, g, h))
+    print('f = {}\ng = {}\nh = {}'.format(f, g, h))
     
     V_1dot = diff(V_1, x_1) * (f + phi * g)
     # Choose phi so \dot{V_1} < 0
-    phi = k_1 * solve(V_1dot < 0, phi).rhs # > TODO: Improve
+    phi = k_1 * solve(V_1dot < 0, phi).rhs # > TODO: Improve phi form guess
     phi_dot = diff(phi, x_1)
+    # Verify that phi holds conditions
     assert x_2 not in phi.free_symbols, 'x_2 should not be in \phi(x_1)'
     assert x_2 not in phi_dot.free_symbols, 'x_2 should not be in \dot{\phi}(x_1)'
     assert phi.subs({x_1: 0, x_2: 0}) == 0, f'phi(0) != 0. phi(0) = {phi.subs({x_1: 0, x_2: 0})}'
@@ -35,10 +37,10 @@ if __name__ == '__main__':
     #z_dot = h - phi_dot * x_1dot #(f + g * x_2)
     
     # > Verify stabilizing input format
-    inp = -k_2*z - (x_1 * g + h - phi_dot * (f + g * x_2))
+    inp = -k_2 * z - (x_1 * g + h - phi_dot * (f + g * x_2))
 
     V_2 = V_1 + V_1.subs(x_1, z)
-    V_2dot = x_1*(f + g*phi) - k_2*z**2
+    V_2dot = x_1 * (f + g * phi) - k_2 * z**2
     V_2dot = V_2dot.subs(u, inp)
     
     # Weak definiteness tests
