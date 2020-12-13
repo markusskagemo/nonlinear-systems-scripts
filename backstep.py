6b6004ca84461c1c9ba976f4f44dbd8ae32309d4
saturdayquant <markusskagemo@gmail.com>
2020-12-13 12:25:27 +0100

    improved latex formatting

--- backstep.py
+++ backstep.py
@@ -3,6 +3,7 @@ from sympy import symbols, diff, exp, simplify, expand, latex, solve, atan
 from sympy import Function, Wild
 
 import matplotlib.pyplot as plt
+import matplotlib
 
 
 u, z, x_1, x_2, z = symbols('u z x_1 x_2, z', real=True)
@@ -12,8 +13,8 @@ f, g, h, V_1, V_2, phi = symbols('f g h V_1 V_2 phi')
 
 if __name__ == '__main__':
     # Specify system and LF
-    x_1dot = 1/exp(x_1) - 2*x_1**3 - 1 + x_2/x_1**2 + x_2*x_1**3
-    x_2dot = -x_2**5 + u + 1 + exp(atan(x_2))
+    x_1dot = x_1 - 2*x_1**3 - 1 + x_2
+    x_2dot = -x_2**5 + u
     V_1 = x_1**2/2
     
     # Infer function components f, g, h from system
@@ -28,19 +29,17 @@ if __name__ == '__main__':
     # Choose phi so \dot{V_1} < 0
     phi = k_1 * solve(V_1dot < 0, phi).rhs # > TODO: Improve phi form guess
     phi_dot = diff(phi, x_1)
+    
     # Verify that phi holds conditions
     assert x_2 not in phi.free_symbols, 'x_2 should not be in \phi(x_1)'
     assert x_2 not in phi_dot.free_symbols, 'x_2 should not be in \dot{\phi}(x_1)'
     assert phi.subs({x_1: 0, x_2: 0}) == 0, f'phi(0) != 0. phi(0) = {phi.subs({x_1: 0, x_2: 0})}'
-
-    # Prepare substitution
-    #z_dot = h - phi_dot * x_1dot #(f + g * x_2)
     
-    # > Verify stabilizing input format
-    inp = -k_2 * z - (x_1 * g + h - phi_dot * (f + g * x_2))
-
+    inp = -k_2 * z - (x_1 * g + h - phi_dot * (f + g * x_2)) # Controlling input
+    z_dot = h + u - phi_dot * x_1dot * (f + g * x_2)
     V_2 = V_1 + V_1.subs(x_1, z)
     V_2dot = x_1 * (f + g * phi) - k_2 * z**2
+    u = u.subs(z, x_2 - phi)
     V_2dot = V_2dot.subs(u, inp)
     
     # Weak definiteness tests
@@ -48,19 +47,33 @@ if __name__ == '__main__':
         raise ValueError('V_dot is not negative def.', V_dot)
     if len(solve(V_2 == 0, (x_1, x_2))) > 1:
         raise ValueError('V is not positive def.', V)
-
-    plt_texts = ['$\phi(x_1) = %s$' % latex(phi)]
-    plt_texts.append('$\dot{\phi}(x_1) = %s$' % latex(phi_dot))
+    
+    plt_texts = ['$f(x_1) = {}$\n$g(x_1) = {}$\n$h(x_2) = {}$'.format(latex(f), latex(g), latex(h))]
+    plt_texts.append('$\phi(x_1) = %s$' % latex(phi))
+    plt_texts.append('$\dot{\phi}(x_1) = %s$\n' % latex(phi_dot))
+    plt_texts.append('Substituting $z = x_2 - \phi(x_1)$, which gives the new system')
+    plt_texts.append('$\dot{x_1} = f(x_1) + g(x_1)\phi(x_1) + g(x_1)z$')
+    plt_texts.append('$\dot{x_1} = %s$' % latex(simplify(f + g * phi + g * (x_2 - phi))))
+    plt_texts.append('$\dot{z} = h(x_2) + u - \partial\phi(x_1)/\partial x_1(f(x_1) +g(x_1)x_2)$')
+    plt_texts.append('$\dot{z} = %s$\n' % latex(simplify(z_dot)))
     plt_texts.append('$u(x_1, x_2) = %s$' % latex(simplify(inp)))
-    plt_texts.append('$u(x_1, x_2) = %s$' % latex(simplify(inp.subs(z, x_2 - phi))))
+    plt_texts.append('$u(x_1, x_2) = %s$\n' % latex(simplify(inp.subs(z, x_2 - phi))))
     plt_texts.append('$V_2 = %s$' % latex(simplify(V_2)))
     plt_texts.append('$V_2 = %s$' % latex(simplify(V_2).subs(z, x_2 - phi)))
-    plt_texts.append('$\dot{V}_2 = %s$' % latex(simplify(V_2dot)))
+    plt_texts.append('$\dot{V}_2 = %s < 0$' % latex(simplify(V_2dot)))
+    plt_texts.append("""
+Since $V_2$ is positive definite and radially unbounded, this makes the origin $(x_1, z) = (0, 0)$ GAS.
+Since $\phi(0) = 0$, the origin of the original system in $(x_1, x_2)$ coincides
+with that of the transformed coordinates and is also GAS.
+    """)
     
     # Render LaTeX
-    ax = plt.axes([0, 0, 0.1, 0.1])
+    matplotlib.rcParams['mathtext.fontset'] = 'stix'
+    matplotlib.rcParams['font.family'] = 'STIXGeneral'
+    ax = plt.axes([0, 0, 0.2, 0.2])
     ax.set_xticks([])
     ax.set_yticks([])
+    plt.rcParams.update({'font.size': 6})
     ax.axis('off')
-    plt.text(0.4, 0.4, '\n\n'.join(plt_texts))
+    plt.text(0.1, 0.1, '\n\n'.join(plt_texts))
     plt.show()
