c0632ce9485d782d6cc37817edcc2dfd619c216f
saturdayquant <markusskagemo@gmail.com>
2020-12-14 20:35:48 +0100

    robustness opt

--- backstep.py
+++ backstep.py
@@ -4,7 +4,9 @@ from sympy import Function, Wild
 
 import matplotlib.pyplot as plt
 import matplotlib
-
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+plt.rcParams.update({'font.size': 6})
 
 u, z, x_1, x_2, z = symbols('u z x_1 x_2, z', real=True)
 k_1, k_2 = symbols('k_1 k_2', positive=True, real=True)
@@ -13,8 +15,8 @@ f, g, h, V_1, V_2, phi = symbols('f g h V_1 V_2 phi')
 
 if __name__ == '__main__':
     # Specify system and LF
-    x_1dot = x_1 - 2*x_1**3 - 1 + x_2
-    x_2dot = -x_2**5 + u
+    x_1dot = x_1/3 + x_2
+    x_2dot = 0#-x_2**2 + u
     V_1 = x_1**2/2
     
     # Infer function components f, g, h from system
@@ -25,21 +27,26 @@ if __name__ == '__main__':
     print('Verify inferred function components:')
     print('f = {}\ng = {}\nh = {}'.format(f, g, h))
     
-    V_1dot = diff(V_1, x_1) * (f + phi * g)
+    V_1dot = diff(V_1, x_1) * x_1dot
     # Choose phi so \dot{V_1} < 0
-    phi = k_1 * solve(V_1dot < 0, phi).rhs # > TODO: Improve phi form guess
+    phi = (k_1 + 1) * solve(V_1dot, x_2)[0]
+    print('Verify inferred \phi:', phi)
     phi_dot = diff(phi, x_1)
+    
     
     # Verify that phi holds conditions
     assert x_2 not in phi.free_symbols, 'x_2 should not be in \phi(x_1)'
     assert x_2 not in phi_dot.free_symbols, 'x_2 should not be in \dot{\phi}(x_1)'
     assert phi.subs({x_1: 0, x_2: 0}) == 0, f'phi(0) != 0. phi(0) = {phi.subs({x_1: 0, x_2: 0})}'
     
-    inp = -k_2 * z - (x_1 * g + h - phi_dot * (f + g * x_2)) # Controlling input
-    z_dot = h + u - phi_dot * x_1dot * (f + g * x_2)
+    inp = -k_2 * z - (x_1 * g + h - phi_dot * (f + g * z)) # Controlling input
+    #z_dot = h + u - phi_dot * x_1dot * (f + g * z)
+    new_x_1dot = simplify(f + g * phi + g * z)
+    z_dot = x_2dot - phi_dot * new_x_1dot #x_1dot.replace(x_2 - phi, z)
+    #z_dot = diff(x_2 - phi, x_1) * x_1dot + diff(x_2 - phi, x_2) * x_2dot
     V_2 = V_1 + V_1.subs(x_1, z)
     V_2dot = x_1 * (f + g * phi) - k_2 * z**2
-    u = u.subs(z, x_2 - phi)
+    #u = u.subs(z, x_2 - phi)
     V_2dot = V_2dot.subs(u, inp)
     
     # Weak definiteness tests
@@ -53,11 +60,12 @@ if __name__ == '__main__':
     plt_texts.append('$\dot{\phi}(x_1) = %s$\n' % latex(phi_dot))
     plt_texts.append('Substituting $z = x_2 - \phi(x_1)$, which gives the new system')
     plt_texts.append('$\dot{x_1} = f(x_1) + g(x_1)\phi(x_1) + g(x_1)z$')
-    plt_texts.append('$\dot{x_1} = %s$' % latex(simplify(f + g * phi + g * (x_2 - phi))))
-    plt_texts.append('$\dot{z} = h(x_2) + u - \partial\phi(x_1)/\partial x_1(f(x_1) +g(x_1)x_2)$')
+    plt_texts.append('$\dot{x_1} = %s$' % latex(new_x_1dot))
+    plt_texts.append('$\dot{z} = h(x_2) + u - \partial\phi(x_1)/\partial x_1(f(x_1) +g(x_1)z)$')
     plt_texts.append('$\dot{z} = %s$\n' % latex(simplify(z_dot)))
-    plt_texts.append('$u(x_1, x_2) = %s$' % latex(simplify(inp)))
-    plt_texts.append('$u(x_1, x_2) = %s$\n' % latex(simplify(inp.subs(z, x_2 - phi))))
+    plt_texts.append('Choosing the control input $u$')
+    plt_texts.append('$u = %s$' % latex(simplify(inp)))
+    plt_texts.append('$u = %s$\n' % latex(simplify(inp.subs(z, x_2 - phi))))
     plt_texts.append('$V_2 = %s$' % latex(simplify(V_2)))
     plt_texts.append('$V_2 = %s$' % latex(simplify(V_2).subs(z, x_2 - phi)))
     plt_texts.append('$\dot{V}_2 = %s < 0$' % latex(simplify(V_2dot)))
@@ -68,12 +76,9 @@ with that of the transformed coordinates and is also GAS.
     """)
     
     # Render LaTeX
-    matplotlib.rcParams['mathtext.fontset'] = 'stix'
-    matplotlib.rcParams['font.family'] = 'STIXGeneral'
     ax = plt.axes([0, 0, 0.2, 0.2])
     ax.set_xticks([])
     ax.set_yticks([])
-    plt.rcParams.update({'font.size': 6})
     ax.axis('off')
     plt.text(0.1, 0.1, '\n\n'.join(plt_texts))
     plt.show()
--- gas.py
+++ gas.py
@@ -1,17 +1,20 @@
 from sympy import diff, simplify, expand, Q, ask, solve
 from sympy import latex, symbols, exp, cos, sin, re, atan, pi
 from sympy import Function, Symbol
+from sympy import S
 from sympy.parsing.latex import parse_latex
 from sympy.matrices import Matrix
 
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 
 x_1, x_2, x_1dot, x_2dot, u, e = symbols('x_1 x_2 x_1dot x_2dot u e', real=True)
 t = Symbol('t', positive=True, real=True)
 V, V_dot = symbols('V V_dot', cls=Function)
-unforced_zero_eq = False
-    
+
     
 def lasalle(x_1dot, x_2dot, V_dot):
     ls_x_1dot = x_1dot
@@ -31,15 +34,36 @@ def lasalle(x_1dot, x_2dot, V_dot):
 
 
 def eq_points(x_1dot, x_2dot):
-    eqp = solve([x_1dot, x_2dot], (x_1, x_2))
+    eqp = None
     unforced_eqp = None
-    print('System eq. points:', eqp)
-    if u in [*x_1dot.free_symbols, *x_2dot.free_symbols]:
+    unforced_zero_eq = False
+    
+    free_1 = []
+    if hasattr(x_1dot, 'free_symbols'):
+        free_1 = x_1dot.free_symbols
+    free_2 = []
+    if hasattr(x_2dot, 'free_symbols'):
+        free_2 = x_2dot.free_symbols
+    
+    if u in [*free_1, *free_2]:
         unforced_eqp = solve([x_1dot.subs(u, 0), x_2dot.subs(u, 0)], (x_1, x_2))
+        if type(unforced_eqp) is dict:
+            unforced_eqp = [tuple(list(unforced_eqp.values()))]
         print('Unforced system eq. points:', unforced_eqp, end='\n\n')
         if '(0, 0)' in [str(eq) for eq in unforced_eqp]:
             unforced_zero_eq = True
-    return eqp, unforced_eqp
+            print('Unforced eq in origin')
+    else:
+        eqp = solve([x_1dot, x_2dot], (x_1, x_2))
+        if type(eqp) is dict:
+            if x_1 not in eqp.keys():
+                eqp[x_1] = S.Zero
+            if x_2 not in eqp.keys():
+                eqp[x_2] = S.Zero
+            eqp = [tuple(list(eqp.values()))]
+            
+        print('System eq. points:', eqp)
+    return eqp, unforced_eqp, unforced_zero_eq
 
 
 def jacobian(x_1dot, x_2dot):
@@ -49,7 +73,7 @@ def jacobian(x_1dot, x_2dot):
     
 def classify_eq_points(eqp, J):
     for eq_x_1, eq_x_2 in eqp:
-        eig_mat = J.subs({x_1: eq_x_1, x_2: eq_x_2})
+        eig_mat = J.subs({x_1: eq_x_1, x_2: eq_x_2, u: 0})
         print(f'Linearized sys. for ({eq_x_1}, {eq_x_2}): A = {eig_mat}')
         eigs_d = eig_mat.eigenvals()
         eigs = eigs_d.keys()
@@ -64,6 +88,10 @@ def classify_eq_points(eqp, J):
             node = 'node'
             saddle = 'Saddle'
         
+        if any(t in eig.free_symbols for eig in eigs):
+            print('Time varying eigenvalues (t). Cannot determine truth value of Relational')
+            print('Manually classify eigs:', eigs)
+            continue
         try:
             eigs = sorted([re(eig) for eig in eigs])
         except Exception as e:
@@ -84,6 +112,7 @@ def classify_eq_points(eqp, J):
                 else:
                     print(f'Class: {saddle} point.\n')
             else:
+                print(eigs)
                 if eigs[0] < eigs[1] < 0:
                     print(f'Class: Stable {node}.\n')
                 elif eigs[1] > eigs[0] > 0:
@@ -94,15 +123,19 @@ def classify_eq_points(eqp, J):
     
 if __name__ == '__main__':
     # Specify system and LF
-    x_1dot = x_1**2 + 1/x_2
-    x_2dot = -x_2**2 + x_1**4
-    V = (x_1**2 + x_2**2)/2
+    x_1dot = -x_1
+    x_2dot = (x_1*x_2 - 1)*x_2**3 + (x_1*x_2 - 1 + x_1**2)*x_2
+    V = x_1**2/2 + x_2**2/2
     
     # Classify equilibrium points
-    eqp, unforced_eqp = eq_points(x_1dot, x_2dot)
+    eqp, unforced_eqp, uze = eq_points(x_1dot, x_2dot)
     J = jacobian(x_1dot, x_2dot)
     print('Jacobian:', simplify(J), end='\n\n')
-    classify_eq_points(eqp, J)
+    if eqp is not None:
+        classify_eq_points(eqp, J)
+    if unforced_eqp is not None:
+        print('Classifying unforced eq. points.')
+        classify_eq_points(unforced_eqp, J)
     
     # \dot{V} algebra for given system
     plt_texts = ['$V = %s$' % latex(V)]
@@ -116,21 +149,25 @@ if __name__ == '__main__':
         raise ValueError('V_dot is not negative def.', V_dot)
     if len(solve(V == 0, (x_1, x_2))) > 1:
         raise ValueError('V is not positive def.', V)
-    if '(0, 0)' in eqp:
+    if eqp is not None and '(0, 0)' in [str(eq) for eq in eqp]:
         print('The origin is stable.', end='\n\n')
 
-    # LaSalle (ls). See H18 problem 2 solution
-    ls_sols, ls_unforced_sols = lasalle(x_1dot, x_2dot, V_dot)
+    try:
+        # LaSalle (ls). See H18 problem 2 solution
+        ls_sols, ls_unforced_sols = lasalle(x_1dot, x_2dot, V_dot)
 
-    print(f'Unique LaSalle solutions: {ls_sols}')
-    if len(ls_sols) == 1 and ('(0.0, 0.0)' in ls_sols or '(0, 0)' in ls_sols):
-        print("""The only solution that can stay identically in S is the trivial solution,
-    and the origin is therefore GAS, assuming radially unbounded V, \dot{V} < 0 and V > 0 (by Corollary 4.2).""")
-    if (unforced_zero_eq and len(ls_unforced_sols) == 1 
-            and ('(0, 0)' in ls_unforced_sols or '(0.0, 0.0)' in ls_sols)):
+        print(f'Unique LaSalle solutions: {ls_sols}')
         print(f'Unique unforced LaSalle solutions: {ls_unforced_sols}')
-        print('The system is GAS for u(t) = 0. Assuming BIBO the system is ISS. (4.21 ISS hint)')
-
+        if len(ls_sols) == 1 and ('(0.0, 0.0)' in ls_sols or '(0, 0)' in ls_sols):
+            print("""The only solution that can stay identically in S is the trivial solution,
+        and the origin is therefore GAS, assuming radially unbounded V, \dot{V} < 0 and V > 0 (by Corollary 4.2).""")
+        if uze:
+            if len(ls_unforced_sols) == 1 and ('(0, 0)' in ls_unforced_sols or '(0.0, 0.0)' in ls_sols):
+                print(f'Unique unforced LaSalle solutions: {ls_unforced_sols}')
+                print('The system is GAS for u(t) = 0. Assuming BIBO the system is ISS. (4.21 ISS hint)')
+            else:
+                print('Since the system is not 0-GAS, it cannot be input-to-state-stable (ISS).')
+    
     ax = plt.axes([0, 0, 0.1, 0.1])
     ax.set_xticks([])
     ax.set_yticks([])
